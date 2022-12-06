5cd6596bd26a8bee6f2367d6c4a9eda50de3e4ad
devfernandoa <fefealzueta@gmail.com>
2022-12-06 18:31:51 -0300

    Conteúdos

---
+++ js/script.js
@@ -0,0 +1,147 @@
+// JAVA != JAVASCRIPT
+
+var index = 0,
+    amount = 0,
+    currTransl = [],
+    translationComplete = true,
+    moveOffset = 0;
+
+var transitionCompleted = function(){
+    translationComplete = true;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    dados = localStorage.getItem('filmes');
+    dados = JSON.parse(dados);
+    console.log(dados);
+
+    titulos = [];
+
+    for(var key in dados) {
+        titulos.push(dados[key]['titulo']);
+    }
+    console.log(titulos)
+    
+    input = document.getElementById("myInput");
+    cabec = document.querySelector('.topnav');
+    recomend = document.createElement('div');
+    recomend.classList.add('filmes');
+    cabec.appendChild(recomend);
+    
+    input.addEventListener("input", function(event) {
+        procura = input.value.toUpperCase();
+        console.clear();
+
+        while (recomend.firstChild) {
+            recomend.removeChild(recomend.firstChild);
+        }
+
+        j = 0;
+        for (i = 0; i < titulos.length; i++) {
+            a = titulos[i].split('.')[0];
+            if (a.toUpperCase().indexOf(procura) > -1 && j < 3) {
+                console.log(a);
+
+                div = document.createElement('div');
+
+                ref = document.createElement('a');
+                ref.href = 'filmes/' + dados[a]['pag'];
+                ref.classList.add('recomend');
+                div.appendChild(ref);
+
+                h1 = document.createElement('p');
+                h1.classList.add("tit");
+                h1.classList.add('busca');
+
+                img = document.createElement('img');
+                img.src = 'img/' + dados[a]['img.src'];
+                img.height = 230;
+                img.width = 130;
+                ref.appendChild(img);
+
+                h1.innerHTML = a;
+                ref.appendChild(h1);
+                recomend.appendChild(div);
+                j+= 1;
+
+            } else {
+                console.log('não encontrado');   
+            } 
+        }
+        if(procura==''){
+            while (recomend.firstChild) {
+                recomend.removeChild(recomend.firstChild);
+            }
+        }
+        
+    });
+
+    var carousel = document.getElementById('carousel');
+
+    amount = document.getElementsByClassName("slide").length;
+    // get the width of the container
+    moveOffset = parseInt(window.getComputedStyle(document.getElementById('carousel-container')).width, 10);
+    // calcuate the width of the carousel
+    document.getElementById('carousel').style.width = (amount * moveOffset) + 'px';
+    // prevent multiple click when transition
+    for(var i = 0; i < amount; i++)
+    {
+        currTransl[i] = -moveOffset;
+        document.getElementsByClassName("slide")[i].addEventListener("transitionend", transitionCompleted, true);                    
+        document.getElementsByClassName("slide")[i].addEventListener("webkitTransitionEnd", transitionCompleted, true);                    
+        document.getElementsByClassName("slide")[i].addEventListener("oTransitionEnd", transitionCompleted, true);                    
+        document.getElementsByClassName("slide")[i].addEventListener("MSTransitionEnd", transitionCompleted, true);                  
+    }
+    // add the last item to the start so that translateX(-moveOffset) works (In case the first click is the previous button)
+    document.getElementById('carousel').insertBefore(document.getElementById('carousel').children[4], document.getElementById('carousel').children[0])
+    // add click events to control arrows
+    document.getElementById('prev').addEventListener('click', prev, true);
+    document.getElementById('next').addEventListener('click', next, true);
+
+});
+
+  function prev()
+{
+    if (translationComplete === true)
+    {
+        translationComplete = false;
+        index--;
+        if (index == -1)
+        {
+            index = amount-1;
+        }
+        var outerIndex = (index) % amount;
+        for (var i = 0; i < amount; i++)
+        {
+            var slide = document.getElementsByClassName("slide")[i];    
+            slide.style.opacity = '1';    
+            slide.style.transform = 'translateX('+(currTransl[i]+moveOffset)+'px)';
+            currTransl[i] = currTransl[i]+moveOffset;
+        }
+        var outerSlide = document.getElementsByClassName("slide")[outerIndex];
+        outerSlide.style.transform = 'translateX('+(currTransl[outerIndex]-(moveOffset*amount))+'px)';
+        outerSlide.style.opacity = '0';
+        currTransl[outerIndex] = currTransl[outerIndex]-moveOffset*(amount);
+    }
+}
+
+function next()
+{
+    if (translationComplete === true)
+    {
+        translationComplete = false;
+        var outerIndex = (index) % amount;
+        index++;
+        for(var i = 0; i < amount; i++)
+        {
+            var slide = document.getElementsByClassName("slide")[i];    
+            slide.style.opacity = '1';    
+            slide.style.transform = 'translateX('+(currTransl[i]-moveOffset)+'px)';
+            currTransl[i] = currTransl[i]-moveOffset;
+        }
+        var outerSlide = document.getElementsByClassName("slide")[outerIndex];
+        outerSlide.style.transform = 'translateX('+(currTransl[outerIndex]+(moveOffset*amount))+'px)';
+        outerSlide.style.opacity = '0';
+        currTransl[outerIndex] = currTransl[outerIndex]+moveOffset*(amount);
+    }
+}
