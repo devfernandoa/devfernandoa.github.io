00a8e615f4b67fec7f23455cd7c05576e2724522
devfernandoa <fefealzueta@gmail.com>
2022-12-06 22:58:19 -0300

    Alterando para atualizar com o repositorio principal

---
+++ js/favorite.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function() {
+    dados = localStorage.getItem('filmes');
+    dados = JSON.parse(dados);
+    console.log(dados);
+
+    body = document.querySelector('body');
+    filme = body.querySelector('h1').innerHTML;
+    
+    main = body.querySelector('main');
+    ul = main.querySelector('ul.linhaaround');
+    btn = ul.querySelector('li.addtofav');
+    img_fav = btn.querySelector('img');
+    console.log(img_fav);
+    
+    
+    if(localStorage.getItem(filme) == null) {
+        console.log('Filme não favoritado');
+        localStorage.setItem(filme, false);
+    }
+
+    console.log(localStorage.getItem(filme));
+    if(localStorage.getItem(filme)) {
+        img_fav.src = '../img/listaativo.png';
+    }
+
+    btn.addEventListener('click', function(event) {
+        event.stopPropagation();
+        if(localStorage.getItem(filme) == 'false') {
+            console.log(filme);
+            localStorage.setItem(filme, true);
+            img_fav.src = '../img/listaativo.png';
+        } else {
+            localStorage.setItem(filme, false);
+            img_fav.src = '../img/addtolist.png';
+        }
+        
+    });
+});
